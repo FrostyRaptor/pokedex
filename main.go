0fe4fc8ad3441882836702b204fcf8bb4bf1b545
FrostyRaptor <e1wink@outlook.com>
2025-01-13 21:36:15 -0600

    Added command line interactions

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleanTextLst := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", cleanTextLst[0])
+	}
 }
